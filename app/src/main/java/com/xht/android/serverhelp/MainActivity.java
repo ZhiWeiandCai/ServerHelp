b6cd4ee8d86acbf0945be214fc73f2f624ad965f
czw <543481852@qq.com>
2016-10-10 17:31:12 +0800

    20161010 new create project

---
+++ app/src/main/java/com/xht/android/serverhelp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xht.android.serverhelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
